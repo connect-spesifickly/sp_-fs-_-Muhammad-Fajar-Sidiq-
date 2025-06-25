2bc7bb2ac9a3bc3af5989e4a21506b88e6a730b3
connect-spesifickly <mozar.sosial@gmail.com>
2025-06-25 19:58:08 +0700

    fix for deploy

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -29,7 +29,10 @@ export default class App {
   private configure(): void {
     this.app.use(cors(corsOptions));
     this.app.options("*", (req, res) => {
-      res.header("Access-Control-Allow-Origin", "https://workase.vercel.app");
+      res.header(
+        "Access-Control-Allow-Origin",
+        "https://sp-fs-muhammad-fajar-sidiq-web-api.vercel.app"
+      );
       res.header(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
