7cd03aaa8e494266792fc9c4fce634d2450dd1c4
connect-spesifickly <mozar.sosial@gmail.com>
2025-06-26 07:52:27 +0700

    add router refresh

--- apps/web/src/components/ui/navbar/main-navbar.tsx
+++ apps/web/src/components/ui/navbar/main-navbar.tsx
@@ -48,11 +48,12 @@ const Navbar = React.forwardRef<
         );
         toast("Project created successfully");
         setOpen(false);
+        router.push("/dashboard");
+        router.refresh();
       } catch (err) {
         toast("Failed to create project");
       } finally {
         setIsSubmitting(false);
-        router.push("/dashboard");
       }
     },
   });
