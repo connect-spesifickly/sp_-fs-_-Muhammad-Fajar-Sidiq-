b1559d45e0cd8097356d46d2d38c8bdec4da3b65
connect-spesifickly <mozar.sosial@gmail.com>
2025-06-26 18:35:28 +0700

    add loading task to dashboard

--- apps/web/src/app/(main)/projects/[id]/_components/kanban/loading-spinner.tsx
+++ apps/web/src/app/(main)/projects/[id]/_components/kanban/loading-spinner.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 
 export default function LoadingSpinner() {
   return (
-    <div className="flex items-center justify-center h-64">
-      <div className="text-lg text-gray-600">Loading tasks...</div>
+    <div className="flex items-center justify-center h-[75vh]">
+      <div className="text-gray-600 text-md">Loading tasks...</div>
     </div>
   );
 }
